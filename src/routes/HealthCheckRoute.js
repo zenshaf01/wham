bfd819b352e8f56b5e06e369a43dfe39f5076f28
zenshaf01 <zeeshanshafqat1@outlook.com>
2025-04-13 09:43:29 -0400

    fixed health check endpoint returning 404

--- src/routes/HealthCheckRoute.js
+++ src/routes/HealthCheckRoute.js
@@ -3,7 +3,7 @@ import express from 'express';
 const router = express.Router();
 
 //Health Check Route
-router.get('/health-check', (req, res) => {
+router.get('/', (req, res) => {
     res.status(200).json({ status: 'OK' });
 });
 
